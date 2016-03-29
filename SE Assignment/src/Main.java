e1f61a6df5650cf91d9f879360c6761163d8134e
BamBo <raksameth@gmail.com>
2016-03-30 00:08:52 +0700

    Add Java SE Assignment Project

---
+++ SE Assignment/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+
+public class Main extends JFrame{
+
+	public static void main(String[] args) {
+		
+		
+		new Main("SE Assignment");
+	}
+	
+	public Main(String Title)
+	{
+		setVisible(true);
+		setSize(200,200);
+		setTitle(Title);
+		
+	}
+
+}
