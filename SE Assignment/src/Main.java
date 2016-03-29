eb670152e6886126a53a9a997d9d3be47b95be34
LengSokpheakdey <sokpheakdeyleng@gmail.com>
2016-03-30 00:21:41 +0700

    Modified Main.java

--- SE Assignment/src/Main.java
+++ SE Assignment/src/Main.java
@@ -1,20 +1,21 @@
+import java.awt.Button;
+
 import javax.swing.JFrame;
 
 
 public class Main extends JFrame{
 
 	public static void main(String[] args) {
-		
-		
 		new Main("SE Assignment");
 	}
 	
 	public Main(String Title)
 	{
+		Button btn = new Button("Hello");
 		setVisible(true);
+		this.add(btn);
 		setSize(200,200);
 		setTitle(Title);
-		
 	}
 
 }
